c81a5c1b9958f947b6335c1e0b6201d0a5ec97f7
FERNANDO BALIANA DE MIRANDA <24122781@CFP127ALUNO.LOCAL>
2025-01-30 12:29:17 -0300

    finalizando

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const botaoTrocarImg = document.getElementById('trocar-img')
+
+const img = [
+    'var(--img1)',
+    'var(--img2)',
+    'var(--img3)',
+    'var(--img4)',
+    
+];
+
+let  induce = 0;
+
+return function(){
+    
+}
+
+
